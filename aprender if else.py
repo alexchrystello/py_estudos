5224aa417b2922bf34a69072e926f96832b2a13d
Alexander Chrystello <alex.chrystello@gmail.com>
2019-08-26 17:58:42 -0300

    primeiras aulas de Phton

---
+++ aprender if else.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 16:37:49 2019
+
+@author: alex
+"""
+
+
+""" Math é biblioteca de matemática,
+onde tem valor de PI"""
+import math
+PI_com_5_casas = round (math.pi,5)
+print (PI_com_5_casas)
+
+
+
+""" Aprender if else"""
+a = input ('Digite o valor de a ')
+b = input ('Digite o valor de b ')
+if b < a:
+    c = 'b é menor do que a'
+else:
+    c = 'b é maior do que a'
+c
+
